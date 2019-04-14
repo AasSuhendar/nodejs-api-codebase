d292f67b0d1429352d46b35809247207933d5774
Aas Suhendar <aas.suhendar@gmail.com>
2019-04-14 13:09:38 +0700

    add env param

--- configs/env.js
+++ configs/env.js
@@ -8,7 +8,7 @@ module.exports = {
     database_mysql_prod: process.env.MYSQL_URL_PROD,
     database_mysql_dev: process.env.MYSQL_URL_DEV,
     database_mysql_test: process.env.MYSQL_URL_TEST,
-    secret_key: process.env.SECRET_KEY,
+    secret_key: process.env.SECRET_KEY || 'SecretKey',
     URL_HOST: process.env.URL_HOST,
     linkHostDomain: process.env.LINK_HOST_DOMAIN,
     swagger_host: process.env.SWAGGER_HOST,
